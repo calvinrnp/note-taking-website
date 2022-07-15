788b1bb9da980f9a6979959c64723d74012d2afb
calvinrnp <calvincrediblee@gmail.com>
2022-07-15 17:15:32 +0700

    add notes function

--- website/models.py
+++ website/models.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
