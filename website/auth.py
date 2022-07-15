dd995305a0c310764a91cfd318fe5431f52f1483
calvinrnp <calvincrediblee@gmail.com>
2022-07-15 16:12:42 +0700

    sign up

--- website/auth.py
+++ website/auth.py
@@ -1,5 +1,10 @@
 import re
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, redirect, render_template, request, flash, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from . import db
+
+from .models import User
 
 auth = Blueprint('auth', __name__)
 
@@ -32,6 +37,11 @@ def sign_up():
         elif len(password1) < 8:
             flash('Password must be longer than 8 characters.', category='error')
         else:
+            new_user = User(email=email, username=username, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit
+
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
 
     return render_template('sign_up.html')
